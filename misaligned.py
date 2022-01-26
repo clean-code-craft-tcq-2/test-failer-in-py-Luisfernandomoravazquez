9e58166bae540c4eb76567a3493d70d0ee541700
mur1ga <mur1ga@rbcm-gerrit.de.bosch.com>
2022-01-25 18:10:06 -0600

    misaligned and tshirts testing improve

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,26 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    color_map_to_Print = ""
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            color_map_to_Print += f'{i * 5 + j} | {major} | {minor}\n'
+    color_map_to_Print = color_map_to_Print[:-1] # Remove last '\n'
+    print(color_map_to_Print)
+    return len(major_colors) * len(minor_colors), color_map_to_Print
 
+result, printedText = print_color_map()
 
-result = print_color_map()
-assert(result == 25)
-print("All is well (maybe!)\n")
+printedText_array = printedText.split("\n")
+assert(result == len(printedText_array))
+index_divisor1_prev = printedText_array[0].find("|")
+index_divisor2_prev = printedText_array[0].rfind("|")
+for zeroBased_row,line in enumerate(printedText_array):
+    index_divisor1 = line.find("|")
+    index_divisor2 = line.rfind("|")
+    assert(str(zeroBased_row+1) == line[:index_divisor1].strip())
+    assert(index_divisor1 == index_divisor1_prev)
+    assert(index_divisor2 == index_divisor2_prev)
+
+print("All is well\n")
--- tshirts.py
+++ tshirts.py
@@ -1,3 +1,10 @@
+MAX_SIZE = 50
+MIN_SIZE = 30
+
+S_M_Umbral_Size = 38
+M_L_Umbral_Size = 42
+
+RECOMEND_BIGGER = True
 
 def size(cms):
     if cms < 38:
@@ -7,8 +14,32 @@ def size(cms):
     else:
         return 'L'
 
+# Test all valid values
+for ZeroBasedShirtSizeinCM in range(MAX_SIZE-MIN_SIZE):
+    # Convert Zero based to size
+    ShirtSizeinCM = ZeroBasedShirtSizeinCM+MIN_SIZE 
+    # Test Small sizes
+    if(ShirtSizeinCM < S_M_Umbral_Size):
+        assert(size(ShirtSizeinCM) == 'S')
+    elif(ShirtSizeinCM == S_M_Umbral_Size):
+        if(RECOMEND_BIGGER is True):
+            assert(size(ShirtSizeinCM) == 'M')
+        else:
+            assert(size(ShirtSizeinCM) == 'S')
+    # Test Medium sizes
+    elif(ShirtSizeinCM < M_L_Umbral_Size):
+        assert(size(ShirtSizeinCM) == 'M')
+    elif(ShirtSizeinCM == M_L_Umbral_Size):
+        if(RECOMEND_BIGGER is True):
+            assert(size(ShirtSizeinCM) == 'L')
+        else:
+            assert(size(ShirtSizeinCM) == 'M')
+    # Test Large sizes
+    else:
+        assert(size(ShirtSizeinCM) == 'L')
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
-print("All is well (maybe!)\n")
+# Test uncoherent values
+assert(size(MIN_SIZE-1) == 'NA')
+assert(size(MAX_SIZE+1) == 'NA')
+
+print("All is well \n")
