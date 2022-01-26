1e91495d0e723fe37bc39907e271c16719a2a095
mur1ga <mur1ga@rbcm-gerrit.de.bosch.com>
2022-01-26 09:18:43 -0600

    Fixing network_alert_stub and testing alerts count

--- alerter.py
+++ alerter.py
@@ -1,13 +1,16 @@
 alert_failure_count = 0
+MAX_TEMP_ALLOWED_IN_CELCIUS = 200
 
 def network_alert_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
+    if(celcius <= MAX_TEMP_ALLOWED_IN_CELCIUS):
     # Return 200 for ok
+        return 200
+    else:
     # Return 500 for not-ok
-    # stub always succeeds and returns 200
-    return 200
+        return 500
 
-def alert_in_celcius(farenheit):
+def alert_in_celcius(farenheit):   # Complain, this function ask for celcius but the parameter is in Farenheit
     celcius = (farenheit - 32) * 5 / 9
     returnCode = network_alert_stub(celcius)
     if returnCode != 200:
@@ -16,10 +19,16 @@ def alert_in_celcius(farenheit):
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 0
+        alert_failure_count += 0 # Error, this lines adds 0, must add 1.
 
-
-alert_in_celcius(400.5)
+# insert 3 valid temperatures
+alert_in_celcius(250.5)
+alert_in_celcius(300.6)
 alert_in_celcius(303.6)
+# insert 3 invalid temperatures
+alert_in_celcius(400.6)
+alert_in_celcius(401.6)
+alert_in_celcius(560.6)
 print(f'{alert_failure_count} alerts failed.')
+assert(alert_failure_count == 3)
 print('All is well (maybe!)')
